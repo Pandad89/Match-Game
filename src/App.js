b7b65f25b202b154430fca71da07dcb1172a1c45
Pandad89 <adambiren19@gmail.com>
2021-12-25 13:09:41 +0200

    Matching Game

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './App.css';
+import TopBar from './components/TopBar';
+import BottomBar from './components/BottomBar';
+import Image from './components/Image';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Correct: 0,
+      Wrong: 0,
+      QuestionNum: 1,
+      Color: [
+        'green',
+        'red',
+        'white',
+        'darkgoldenrod',
+        'crimson',
+      ],
+      ButtonText: [
+        'Correct',
+        'Wrong',
+      ],
+      ColorIndex: 0,
+      BackgroundColor: [
+        'darkgreen',
+        'coral',
+        'black',
+        'chartreuse',
+        'cornflowerblue',
+        'darkred',
+        'goldenrod',
+        'hotpink',
+        'orangered',
+        'saddlebrown',
+        'burlywood',
+        'cadetblue',
+        'darkmagenta',
+        'lavender',
+        'mediumseagreen',
+        'thistle',
+        'rosybrown',
+        'teal',
+        'sienna',
+        'salmon',
+        'palevioletred',
+        'darkviolet',
+        'olive',
+        'indigo',
+        'aquamarine',
+      ],
+      EndGame: false,
+    }
+  }
+
+  correctClickHandler = () => {
+    if (this.state.QuestionNum <= 25) {
+      this.setState({
+        Correct: this.state.Correct += 1,
+        QuestionNum: this.state.QuestionNum += 1,
+        ColorIndex: this.state.ColorIndex += 1,
+      })
+    }
+  }
+  wrongClickHandler = () => {
+    this.setState({
+      Wrong: this.state.Wrong += 1,
+      QuestionNum: this.state.QuestionNum += 1,
+      ColorIndex: this.state.ColorIndex += 1,
+    })
+  }
+
+  render() {
+    return (
+      <div className='Background'>
+        <div className="AppBody">
+          <TopBar Correct={this.state.Correct} Wrong={this.state.Wrong} Color={this.state.Color} />
+          <Image Question={this.state.QuestionNum} Color={this.state.Color} BackgroundColor={this.state.BackgroundColor} ColorIndex={this.state.ColorIndex} />
+          <BottomBar ButtonText={this.state.ButtonText} Click={[this.correctClickHandler, this.wrongClickHandler]} Color={this.state.Color}/>
+        </div>
+      </div>
+    )
+  }
+}
+export default App;
